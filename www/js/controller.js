2cb21f3ae906c17e1f0570be1b7a663175bc31d0
priya-logiciel <87015564+priya-logiciel@users.noreply.github.com>
2021-07-29 18:57:03 -0700

    my task is not completed

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,67 +1,64 @@
 (function(){
 	'use strict';	
-		var DemoCtrl = function($scope)  {
-			$scope.names = [
+		var DemoCtrl = function($scope,$ionicPopup)  {
+			$scope.datalist = [
 				
 				{
+					id:'1',
 					firstName: 'Mayank',
 					dob: '12-12-1990',
-					gender: 'male',
-					hobbies: 'Gaming',
 					firstCompanyName: 'LS',
 					lastName: 'Kumar',
 					salary: '250000',
 					age: '31',
 				},
-				{
+				{  
+					id:'2',
 					firstName: 'Tajinder',
 					dob: '12-12-1993',
-					gender: 'male',
-					hobbies: 'Gaming, Googling',
 					firstCompanyName: 'Promatics',
 					lastName: 'Singh',
 					salary: '2500000',
 					age: '28',
 				},
-				{
+				{   
+					id:'3',
 					firstName: 'Varsha',
 					dob: '12-12-1995',
-					gender: 'female',
-					hobbies: 'Singing',
 					firstCompanyName: 'LS',
 					lastName: 'Panday',
 					salary: '2500',
 					age: '20',
 				},
 				{
+					id:'4',
 					firstName: 'Pawan',
 					dob: '07-01-1995',
-					gender: 'male',
-					hobbies: 'Gardning',
 					firstCompanyName: 'LS',
 					lastName: 'Arora',
 					salary: '550000',
 					age: '25',
 				},
 				{
+					id:'5',
 					firstName: 'Anuj',
 					dob: '12-11-1997',
-					gender: 'male',
-					hobbies: 'Gaming',
 					firstCompanyName: 'LS',
 					lastName: 'Singh',
 					salary: '2500000',
 					age: '29',
 				},
 			]
-			  $scope.orderByMe = function (x) {
-				$scope.myOrderBy = x;
-			  };
-	}
-			DemoCtrl.$inject = ['$scope' ]
+			$scope.dataToShow=''
+            
+			$scope.showDataInPopup = function(){
+				console.log ($scope.dataToShow)
+			}
+
+				
+			}  
+			DemoCtrl.$inject = ['$scope','$ionicPopup' ]
 			angular
 				.module('starter')
 				.controller('MainCtrl',DemoCtrl);
 })(); 
-	   
-
